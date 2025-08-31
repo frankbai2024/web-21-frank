8f29c686a78be89078d036f394141c1aa29f3372
haixin zhang <haixin.zhang@go1.com>
2025-08-31 20:17:54 +0800

    Web project 2

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# web-21
+# WEB-PROJECT
+
+## How to use:
+
+1. Clone the repo
+
+
+### frontend
+
+```
+cd frontend
+
+npm install
+
+npm run dev
+```
+
+### backend
+
+```
+cd backend
+
+npm install
+
+npm run dev
+```
+
+Project will be running on `http://localhost:8080/`
---
+++ frontend/src/js/database/utils.js
@@ -0,0 +1,59 @@
+import path from "path-browserify";
+import axios from "axios";
+
+export const postData = async (path, data) => {
+    try {
+        await axios.post(path, data, {
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error("Error posting data:", error);
+    }
+}
+
+export const deleteData = async (path) => {
+    try {
+        await axios.delete(path);
+    } catch (error) {
+        console.error("Error deleting data:", error);
+    }
+}
+
+export const putData = async (path, data) => { 
+    try {
+        await axios.put(path, data, {
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error("Error updating data:", error);
+    }
+}
+
+export const patchData = async (path, data) => {
+    try {
+        await axios.patch(path, data, {
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        console.error("Error patching data:", error);
+    }
+}
+
+export const getData = async (path) => {
+    try {
+        const response = await axios.get(path);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+export const getResourcePath = (pathInfo) => {
+    const dbPath = import.meta.env.VITE_LOCAL_DB_PATH;
+    const protocol = 'http://';
+    // http://localhost:80/api/{resource}
+
+    // pathInfo = [userPosts, userPostsComments, ]
+    // http://localhost:80/api/userPosts/userPostsComments
+    return `${protocol}${dbPath}/api/${path.join(pathInfo, '/')}`;
+}
